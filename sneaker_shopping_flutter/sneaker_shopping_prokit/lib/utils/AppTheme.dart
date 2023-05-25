1fb4001e23e798ac039fe1446bd20c75c434ccf6
JOSE <luisjose2210@gmail.com>
2023-05-24 21:10:38 -0600

    Plantilla flutter

---
+++ sneaker_shopping_flutter/sneaker_shopping_prokit/lib/utils/AppTheme.dart
@@ -0,0 +1,106 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:sneaker_shopping_prokit/utils/SSColors.dart';
+
+class AppThemeData {
+  //
+  AppThemeData._();
+
+  static final ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: whiteColor,
+    primaryColor: appColorPrimary,
+    primaryColorDark: appColorPrimary,
+    errorColor: Colors.red,
+    hoverColor: Colors.white54,
+    dividerColor: viewLineColor,
+    fontFamily: GoogleFonts.openSans().fontFamily,
+    appBarTheme: AppBarTheme(
+      color: appLayout_background,
+      iconTheme: IconThemeData(color: textPrimaryColor),
+      systemOverlayStyle: SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
+    ),
+    textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.black),
+    colorScheme: ColorScheme.light(primary: appColorPrimary, primaryVariant: appColorPrimary),
+    cardTheme: CardTheme(color: Colors.white),
+    cardColor: Colors.white,
+    iconTheme: IconThemeData(color: textPrimaryColor),
+    bottomSheetTheme: BottomSheetThemeData(backgroundColor: whiteColor),
+    textTheme: TextTheme(
+      button: TextStyle(color: appColorPrimary),
+      headline6: TextStyle(color: textPrimaryColor),
+      subtitle2: TextStyle(color: textSecondaryColor),
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  ).copyWith(
+    pageTransitionsTheme: PageTransitionsTheme(builders: <TargetPlatform, PageTransitionsBuilder>{
+      TargetPlatform.android: OpenUpwardsPageTransitionsBuilder(),
+      TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+      TargetPlatform.linux: OpenUpwardsPageTransitionsBuilder(),
+      TargetPlatform.macOS: OpenUpwardsPageTransitionsBuilder(),
+    }),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: appBackgroundColorDark,
+    highlightColor: appBackgroundColorDark,
+    errorColor: Color(0xFFCF6676),
+    appBarTheme: AppBarTheme(
+      color: appBackgroundColorDark,
+      iconTheme: IconThemeData(color: blackColor),
+      systemOverlayStyle: SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
+    ),
+    primaryColor: color_primary_black,
+    dividerColor: Color(0xFFDADADA).withOpacity(0.3),
+    primaryColorDark: color_primary_black,
+    textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.white),
+    hoverColor: Colors.black12,
+    fontFamily: GoogleFonts.openSans().fontFamily,
+    bottomSheetTheme: BottomSheetThemeData(backgroundColor: appBackgroundColorDark),
+    primaryTextTheme: TextTheme(headline6: primaryTextStyle(color: Colors.white), overline: primaryTextStyle(color: Colors.white)),
+    cardTheme: CardTheme(color: cardBackgroundBlackDark),
+    cardColor: appSecondaryBackgroundColor,
+    iconTheme: IconThemeData(color: whiteColor),
+    textTheme: TextTheme(
+      button: TextStyle(color: color_primary_black),
+      headline6: TextStyle(color: whiteColor),
+      subtitle2: TextStyle(color: Colors.white54),
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    colorScheme: ColorScheme.dark(primary: appBackgroundColorDark, onPrimary: cardBackgroundBlackDark, primaryVariant: color_primary_black).copyWith(secondary: whiteColor),
+  ).copyWith(
+    pageTransitionsTheme: PageTransitionsTheme(builders: <TargetPlatform, PageTransitionsBuilder>{
+      TargetPlatform.android: OpenUpwardsPageTransitionsBuilder(),
+      TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+      TargetPlatform.linux: OpenUpwardsPageTransitionsBuilder(),
+      TargetPlatform.macOS: OpenUpwardsPageTransitionsBuilder(),
+    }),
+  );
+}
+
+Widget commonCacheImageWidget(String? url, {double? height, double? width, BoxFit? fit}) {
+  if (url.validate().startsWith('http')) {
+    if (isMobile) {
+      return CachedNetworkImage(
+        placeholder: placeholderWidgetFn() as Widget Function(BuildContext, String)?,
+        imageUrl: '$url',
+        height: height,
+        width: width,
+        fit: fit ?? BoxFit.cover,
+        errorWidget: (_, __, ___) {
+          return SizedBox(height: height, width: width);
+        },
+      );
+    } else {
+      return Image.network(url!, height: height, width: width, fit: fit ?? BoxFit.cover);
+    }
+  } else {
+    return Image.asset(url!, height: height, width: width, fit: fit ?? BoxFit.cover);
+  }
+}
+
+Widget? Function(BuildContext, String) placeholderWidgetFn() => (_, s) => placeholderWidget();
+
+Widget placeholderWidget() => Image.asset('images/mpmusic/placeholder.jpg', fit: BoxFit.cover);
